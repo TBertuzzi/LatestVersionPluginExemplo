16f8e484687b26c2de0e8b209a465ca895ebc531
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-03-20 10:46:24 -0300

    Escrevendo o Artigo

--- LatestVersionPluginExemplo/LatestVersionPluginExemplo/MainPage.xaml.cs
+++ LatestVersionPluginExemplo/LatestVersionPluginExemplo/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Plugin.LatestVersion;
 
 namespace LatestVersionPluginExemplo
 {
@@ -12,6 +13,26 @@ namespace LatestVersionPluginExemplo
 		public MainPage()
 		{
 			InitializeComponent();
-		}
+
+            btnVersao.Clicked += async (sender, e) => {
+
+                //Verifica a versão atual do APP
+                var isLatest = await CrossLatestVersion.Current.IsUsingLatestVersion();
+
+                if (!isLatest)
+                {
+                    //Avisa o usuario que existe uma versão nova e pergunta se ele quer baixar
+                    var update = await DisplayAlert("Nova versão disponivel", "Existe uma nova versão deste app,deseja atualizar?", "Sim", "Não");
+
+                    if (update)
+                    {
+                        //Abre a Loja nativa da plataforma para efetur o Download
+                        await CrossLatestVersion.Current.OpenAppInStore();
+                    }
+                }
+            };
+
+            
+        }
 	}
 }
